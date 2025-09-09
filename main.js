6b6c427083242bc487587c9b8749f3895a1ffbb9
Rosano Coutinho <github.com@xeari.com>
2025-09-09 13:24:59 +0100

    update site url

--- main.js
+++ main.js
@@ -29,7 +29,7 @@
 <p>Quarterly updates on projects, writing, music, links, and life, right in your inbox.</p>
 
 <div class="ROCOBulletin">
-  <script src="https://cdn.jsdelivr.net/ghost/signup-form@~0.3/umd/signup-form.min.js" data-button-color="#424242" data-button-text-color="#FFFFFF" data-site="https://utopia.rosano.ca/" data-locale="en" async></script>
+  <script src="https://cdn.jsdelivr.net/ghost/signup-form@~0.3/umd/signup-form.min.js" data-button-color="#424242" data-button-text-color="#FFFFFF" data-site="https://occasion.rosano.ca/" data-locale="en" async></script>
 </div>
 
 <p class="OLSKFollow">Or more frequently on <a class="OLSKFollowMastodon" target="_blank" href="https://rosano.ca/mastodon">Mastodon</a>, <a class="OLSKFollowBluesky" target="_blank" href="https://rosano.ca/bluesky">Bluesky</a>, and <a href="https://rosano.ca/log">Journal</a>.
